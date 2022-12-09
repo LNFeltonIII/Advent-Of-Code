e06b6d09ea3a4deab03bf440bc50bcd38384b026
LNFeltonIII <43047629+LNFeltonIII@users.noreply.github.com>
2022-12-08 20:21:39 -0600

    Add files via upload

---
+++ 2022/Day7.ps1
@@ -0,0 +1,115 @@
+﻿#I'll solve this by doing a small reproduction of objects obtained from the commandlets Get-Item and Get-ChildItem. I will build objects which will have the following properties for each line item which indicates command output:
+<#
+FullName          : C:\Users\Neal\Documents\PowerShell\Scripts\AdventOfCode\2022\day7_input.txt
+ParentPath        : C:\Users\Neal\Documents\PowerShell\Scripts\AdventOfCode\2022
+IsContainer       : False             
+Name              : day7_input.txt
+Length            : 13172
+#>
+Measure-Command {
+$PuzzleInput = Get-Content -Path C:\Users\Neal\Documents\PowerShell\Scripts\AdventOfCode\2022\day7_input.txt -Encoding UTF8
+
+#First we'll process the input file to create workable filesystem objects for querying. We'll start by manually adding the root.
+$RootFileSystemObject = [PSCustomObject]@{
+    FullName = "/"
+    ParentPath = ''
+    IsContainer = $true
+    Name = "/"
+    Length = 0
+}
+
+$FileSystemObjects = @()
+$FileSystemObjects += $RootFileSystemObject
+$CurrentDirectory = ''
+
+#Now we'll parse the puzzle input file
+for($Iterator = 1; $Iterator -lt $PuzzleInput.Length; $Iterator++) {
+    $LineItem = $PuzzleInput[$Iterator]
+
+    #Check to see if it's a Change Directory command and update the directory accordingly
+    if($LineItem -match '\$ cd ' ) {
+        $DirectoryToChangeTo = ($LineItem -split '\$ cd ')[1]
+        
+        if($DirectoryToChangeTo -like "..") {$CurrentDirectory = ($CurrentDirectory -split '/' | Select-Object -SkipLast 1) -join '/'}
+        else {$CurrentDirectory += "/$DirectoryToChangeTo"}
+    }
+
+    #Check to see if it's a List command and create file system objects accordingly. In this case, after determining it's a list command, we'll continue to traverse the file until we stumble upon the next command or the end of the file.
+    elseif($LineItem -match '\$ ls') {
+        $Iterator++
+        $LineItem = $PuzzleInput[$Iterator]
+
+        while($LineItem -notmatch '\$' -and $LineItem -notlike $null) {
+            $Name = ($LineItem -split ' ')[1]
+            $IsContainer = $LineItem -like 'dir *'
+
+            if($CurrentDirectory -like $null) {
+                $ParentPath = "/"
+                $FullName = $ParentPath + "$Name"
+            }
+
+            else {
+                $ParentPath = $CurrentDirectory
+                $FullName = $ParentPath + "/$Name"
+            }
+            
+            if($IsContainer) {[int]$Length = 0}
+            else {[int]$Length = ($LineItem -split ' ')[0]}
+            
+            $FileSystemObject = [PSCustomObject]@{
+                FullName = $FullName
+                ParentPath = $ParentPath
+                IsContainer = $IsContainer
+                Name = $Name
+                Length = $Length
+            }
+
+            $FileSystemObjects += $FileSystemObject
+
+            #Since Directories don't typically have a Length, we're going to use that property to hold the total of all files contained within the directory by adding to it each time a file it contains is encountered.
+            ($FileSystemObjects | Where-Object -Property FullName -Like $FileSystemObject.ParentPath).Length += $FileSystemObject.Length
+
+            $Iterator++
+            $LineItem = $PuzzleInput[$Iterator]
+        }
+
+        $Iterator--
+
+    }
+
+}
+
+#Now add the total amount consumed by each directory to it's parent directory from the bottom to the top.
+for($Iterator = $FileSystemObjects.Count - 1; $Iterator -gt 0; $Iterator--) {
+    $FileSystemObject = $FileSystemObjects[$Iterator]
+    if($FileSystemObject.IsContainer -like $true) {($FileSystemObjects | Where-Object -Property FullName -Like $FileSystemObject.ParentPath).Length += $FileSystemObject.Length}
+}
+
+#Then sum up the answer. The answer is the Sum.
+$FileSystemObjects | Where-Object {$_.IsContainer -like $true -and $_.Length -le 100000} | Measure-Object -Property Length -Sum
+<#
+Count    : 34
+Average  : 
+Sum      : 1783610
+Maximum  : 
+Minimum  : 
+Property : Length
+#>
+}
+#Part2
+#First we need to find the current free space. This will be as simple as adding up all the space used by the files, which we can tell are files if IsContainer is False, and subtracting it from the total space given.
+$CurrentFreeSpace = 70000000 - ($FileSystemObjects | Where-Object -Property IsContainer -Like $false | Measure-Object -Property Length -Sum).Sum
+#25640133
+$ExtraFreeSpaceRequired = 30000000 - $CurrentFreeSpace
+#4359867
+
+#Now, since we already performed the directory calculations, we simply select the ones which will ensure that the extra free space obtained is greater than or equal to what's required, sort them, and select the first one.
+#The Length of that directory is the answer.
+$FileSystemObjects | Where-Object {$_.IsContainer -like $true -and $_.Length -ge $ExtraFreeSpaceRequired} | Sort-Object -Property Length | Select-Object -First 1
+<#
+FullName    : /nfn/qmrsvfvw/fpljqj/tdnp
+ParentPath  : /nfn/qmrsvfvw/fpljqj
+IsContainer : True
+Name        : tdnp
+Length      : 4370655
+#>
---
+++ 2022/Day8.ps1
@@ -0,0 +1,171 @@
+﻿#I'm going to solve this one with a multidimensional array so that the lookups will be an easy to read line
+
+$PuzzleInput = Get-Content -Path C:\Users\Neal\Documents\PowerShell\Scripts\AdventOfCode\2022\day8_input.txt -Encoding UTF8
+$NumberOfTreesNorthToSouth = $PuzzleInput.Count
+$NumberOfTreesEastToWest = $PuzzleInput[0].Length
+[PSCustomObject[,]]$Forest = [PSCustomObject[,]]::new($NumberOfTreesNorthToSouth,$NumberOfTreesEastToWest)
+
+#Populate the forest with tree objects containing coordinates, tree height, and direction of visibility (from the perspective of the tree, this states from which direction the tree can be seen from the outer edge).
+for($RowOfTrees = 0; $RowOfTrees -lt $NumberOfTreesNorthToSouth; $RowOfTrees++) {
+    for($IndividualTree = 0; $IndividualTree -lt $NumberOfTreesEastToWest; $IndividualTree++) {
+        $Height = $PuzzleInput[$RowOfTrees][$IndividualTree]
+        
+        [PSCustomObject]$TreeInForest = @{
+            Coordinates = [int]$RowOfTrees,[int]$IndividualTree
+            Height = $Height
+            DirectionOfVisibility = [System.Collections.Generic.List[string]]@()
+            ScenicScore = 0
+        }
+
+        $Forest[$RowOfTrees,$IndividualTree] = $TreeInForest
+                
+    }
+
+}
+
+function Verify-TreeVisibility ($TreeInForest, $Perspective, $NumberOfTreesNorthToSouth, $NumberOfTreesEastToWest) {
+
+    if($Perspective -like "North") {
+        #Any tree in the first row is visible from the North.
+        if($TreeInForest.Coordinates[0] -eq 0) {$IsVisible = $true}
+        
+        #Otherwise, we must actually look at the rest of the trees to find out. Is there a tree larger than the current tree in the current tree's column between the current tree and the edge?
+        else {
+            $OuterEdge = 0
+            $CurrentTreeColumn = $TreeInForest.Coordinates[1]
+            $IsVisible = ($OuterEdge..($TreeInForest.Coordinates[0] - 1) | ForEach-Object {$Forest[$_,$CurrentTreeColumn]} | Where-Object {$_.Height -ge $TreeInForest.Height}).Count -eq 0
+        }
+
+        if($IsVisible) {$Forest[$TreeInForest.Coordinates[0],$TreeInForest.Coordinates[1]].DirectionOfVisibility.Add("North")}
+    }
+    
+    elseif($Perspective -like "South") {
+        #Any tree in the last row is visible from the South.
+        if($TreeInForest.Coordinates[0] -eq ($NumberOfTreesNorthToSouth - 1)) {$IsVisible = $true}
+
+        #Otherwise, we must actually look at the rest of the trees to find out. Is there a tree larger than the current tree in the current tree's column between the current tree and the edge?
+        else {
+            $OuterEdge = $NumberOfTreesNorthToSouth - 1
+            $CurrentTreeColumn = $TreeInForest.Coordinates[1]
+            $IsVisible = ($OuterEdge..($TreeInForest.Coordinates[0] + 1) | ForEach-Object {$Forest[$_,$CurrentTreeColumn]} | Where-Object {$_.Height -ge $TreeInForest.Height}).Count -eq 0
+        
+        }
+
+        if($IsVisible) {$Forest[$TreeInForest.Coordinates[0],$TreeInForest.Coordinates[1]].DirectionOfVisibility.Add("South")}
+
+    }
+
+    elseif($Perspective -like "East") {
+        #Any tree in the last column is visible from the East.
+        if($TreeInForest.Coordinates[1] -eq ($NumberOfTreesEastToWest - 1)) {$IsVisible = $true}
+
+        #Otherwise, we must actually look at the rest of the trees to find out. Is there a tree larger than the current tree in the current tree's row between the current tree and the edge?
+        else {
+            $OuterEdge = $NumberOfTreesEastToWest - 1
+            $CurrentTreeRow = $TreeInForest.Coordinates[0]
+            $IsVisible = ($OuterEdge..($TreeInForest.Coordinates[1] + 1) | ForEach-Object {$Forest[$CurrentTreeRow,$_]} | Where-Object {$_.Height -ge $TreeInForest.Height}).Count -eq 0
+        
+        }
+
+        if($IsVisible) {$Forest[$TreeInForest.Coordinates[0],$TreeInForest.Coordinates[1]].DirectionOfVisibility.Add("East")}
+
+    }
+
+    elseif($Perspective -like "West") {
+        #Any tree in the first column is visible from the West.
+        if($TreeInForest.Coordinates[1] -eq 0) {$IsVisible = $true}
+
+        #Otherwise, we must actually look at the rest of the trees to find out. Is there a tree larger than the current tree in the current tree's row between the current tree and the edge?
+        else {
+            $OuterEdge = 0
+            $CurrentTreeRow = $TreeInForest.Coordinates[0]
+            $IsVisible = ($OuterEdge..($TreeInForest.Coordinates[1] - 1) | ForEach-Object {$Forest[$CurrentTreeRow,$_]} | Where-Object {$_.Height -ge $TreeInForest.Height}).Count -eq 0
+        
+        }
+
+        if($IsVisible) {$Forest[$TreeInForest.Coordinates[0],$TreeInForest.Coordinates[1]].DirectionOfVisibility.Add("West")}
+
+    }
+
+}
+
+$Perspectives = "North","South","East","West"
+#Populate the DirectionOfVisibility property of each tree
+foreach($Perspective in $Perspectives) {
+    foreach($TreeInForest in $Forest) {
+        Verify-TreeVisibility -TreeInForest $TreeInForest -Perspective $Perspective -NumberOfTreesNorthToSouth $NumberOfTreesNorthToSouth -NumberOfTreesEastToWest $NumberOfTreesEastToWest
+    }
+}
+
+
+#Now just count the number of objects where the property DirectionOfVisibility is not empty.
+#The Count is the answer.
+$Forest | Where-Object {$_.DirectionOfVisibility.Count -ne 0} | Measure-Object
+<#
+Count    : 1688
+Average  : 
+Sum      : 
+Maximum  : 
+Minimum  : 
+Property : 
+#>
+
+function Calculate-ScenicScore ($TreeInForest, $NumberOfTreesNorthToSouth, $NumberOfTreesEastToWest) {
+    
+    #No calculations required if it's on the edge
+    if($TreeInForest.Coordinates[0] -eq 0) {return 0}
+    elseif($TreeInForest.Coordinates[0] -eq ($NumberOfTreesNorthToSouth - 1)) {return 0}
+    elseif($TreeInForest.Coordinates[1] -eq ($NumberOfTreesEastToWest - 1)) {return 0}
+    elseif($TreeInForest.Coordinates[1] -eq 0) {return 0}
+
+    #Up
+    $OuterEdge = 0
+    $StartingRow = $TreeInForest.Coordinates[0] - 1
+    $CurrentTreeColumn = $TreeInForest.Coordinates[1]
+
+    for($UpScenicScore = 0; ($StartingRow - $UpScenicScore) -ge $OuterEdge; $UpScenicScore++) {
+
+        if($TreeInForest.Height -le $Forest[($StartingRow - $UpScenicScore),$CurrentTreeColumn].Height) {$UpScenicScore++; break}
+    }
+   
+    #Down
+    $OuterEdge = $NumberOfTreesNorthToSouth - 1
+    $StartingRow = $TreeInForest.Coordinates[0] + 1
+    $CurrentTreeColumn = $TreeInForest.Coordinates[1]
+
+    for($DownScenicScore = 0; ($StartingRow + $DownScenicScore) -le $OuterEdge; $DownScenicScore++) {
+  
+        if($TreeInForest.Height -le $Forest[($StartingRow + $DownScenicScore),$CurrentTreeColumn].Height) {$DownScenicScore++; break}
+    
+    }
+
+    #Left
+    $OuterEdge = 0
+    $StartingColumn = $TreeInForest.Coordinates[1] - 1
+    $CurrentTreeRow = $TreeInForest.Coordinates[0]
+
+    for($LeftScenicScore = 0; ($StartingColumn - $LeftScenicScore) -ge $OuterEdge; $LeftScenicScore++) {
+        
+        if($TreeInForest.Height -le $Forest[$CurrentTreeRow,($StartingColumn - $LeftScenicScore)].Height) {$LeftScenicScore++; break}
+    }
+
+    #Right
+    $OuterEdge = $NumberOfTreesEastToWest - 1
+    $StartingColumn = $TreeInForest.Coordinates[1] + 1
+    $CurrentTreeRow = $TreeInForest.Coordinates[0]
+
+    for($RightScenicScore = 0; ($StartingColumn + $RightScenicScore) -le $OuterEdge; $RightScenicScore++) {
+        
+        if($TreeInForest.Height -le $Forest[$CurrentTreeRow,($StartingColumn + $RightScenicScore)].Height) {$RightScenicScore++; break}
+    }
+
+    return $UpScenicScore * $DownScenicScore * $LeftScenicScore * $RightScenicScore
+}
+
+#Part2
+#Calculate the Scenic Scores
+$Forest | ForEach-Object { $_.ScenicScore = Calculate-ScenicScore -TreeInForest $_ -NumberOfTreesNorthToSouth $NumberOfTreesNorthToSouth -NumberOfTreesEastToWest $NumberOfTreesEastToWest }
+
+#Select the one which is the highest
+$Forest.ScenicScore | Sort-Object -Descending | Select-Object -First 1
+#410400
